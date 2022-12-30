4e05dec81ac11d7fb4f1aa01285f2bdf7bf0fead
arty <artesan538@gmail.com>
2022-12-30 18:31:43 +0600

    host role, filebeat role, nginx and app playbook

---
+++ kafka-04/terraform/app.tf
@@ -0,0 +1,51 @@
+resource "yandex_compute_instance" "app" {
+    count = "${var.app_count}"
+    name    = "app-${count.index}"
+
+    resources {
+        cores  = 2
+        memory = 2
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id = "${data.yandex_compute_image.ubuntu_image.id}" #ubuntu-22-04-lts-v20220815
+            size = 15
+        }
+    }
+
+    network_interface {
+        subnet_id = "${data.yandex_vpc_subnet.subnet.id}" # default-ru-central1-b [10.129.0.0/24]
+        nat = true
+    }
+
+    metadata = {
+        user-data = "${file("${path.module}/user-data")}"
+    }
+}
+
+resource "yandex_compute_instance" "nginx" {
+    count = "${var.nginx_count}"
+    name    = "nginx-${count.index}"
+
+    resources {
+        cores  = 2
+        memory = 2
+    }
+
+    boot_disk {
+        initialize_params {
+            image_id = "${data.yandex_compute_image.ubuntu_image.id}" #ubuntu-22-04-lts-v20220815
+            size = 15
+        }
+    }
+
+    network_interface {
+        subnet_id = "${data.yandex_vpc_subnet.subnet.id}" # default-ru-central1-b [10.129.0.0/24]
+        nat = true
+    }
+
+    metadata = {
+        user-data = "${file("${path.module}/user-data")}"
+    }
+}
--- kafka-04/terraform/var.tf
+++ kafka-04/terraform/var.tf
@@ -3,3 +3,15 @@ variable "kafka_count" {
   type        = number
   default     = "3"
 }
+
+variable "app_count" {
+  description = "Amount of hosts to run"
+  type        = number
+  default     = "1"
+}
+
+variable "nginx_count" {
+  description = "Amount of hosts to run"
+  type        = number
+  default     = "1"
+}
